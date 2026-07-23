7e7ce41f78c525119eea1280fa3be7eb11290544
Volodymyr M <vovchara@ukr.net>
2026-07-23 09:09:14 +0200

    tab ordering fix

--- src/fuzzy/systems/index.ts
+++ src/fuzzy/systems/index.ts
@@ -3,7 +3,7 @@ import { aggregationSystem } from "./aggregation";
 import { commCtrlSystem } from "./commCtrl";
 import { routingSystem } from "./routing";
 
-export const systems: readonly FuzzySystem[] = [commCtrlSystem, routingSystem, aggregationSystem];
+export const systems: readonly FuzzySystem[] = [commCtrlSystem, aggregationSystem, routingSystem];
 
 export function findSystem(id: string): FuzzySystem | undefined {
   return systems.find((s) => s.id === id);
